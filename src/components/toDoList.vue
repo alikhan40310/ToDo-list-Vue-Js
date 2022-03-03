28bd8a5bd55a5c92862c3dfad46743d68d859bf6
Ali Khan <alikhan40310@gmail.com>
2022-03-03 20:46:04 +0500

    setup todolist by bootStrap

---
+++ src/components/toDoList.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+
+ 
+</script>
+
+<template>
+
+    <div class="container">
+        <h2>My ToDo App</h2>
+        <div class="d-flex gap-2">
+            <input type="text" placeholder="Enter Value" class="form-control">
+            <button class="btn btn-warning rounded-1">submit</button>
+        </div>
+        <!-- using bootstrap table -->
+        <table class="table table-bordered mt-5">
+            <thead>
+                <tr>
+                    <th scope="col">Task</th>
+                    <th scope="col">Status</th>
+                    <th scope="col" class="text-center">#</th>
+                    <th scope="col" class="text-center">#</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>steal bananas from the store</td>
+                    <td>To-do</td>
+                    <td>
+                        <div>
+                            <span class="fa fa-pen"></span>
+                        </div>
+                    </td>
+                    <td> </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+</template>
+
+<style scoped>
+    .ItemCenter{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// font awesome attachement
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).mount('#app')
+library.add(faHatWizard)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
