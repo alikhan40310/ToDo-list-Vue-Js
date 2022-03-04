c5c06551fe5d3a1f7c0184dc34abc89778274bd3
Ali Khan <alikhan40310@gmail.com>
2022-03-04 15:32:45 +0500

    on click submit apend item in table

--- src/components/toDoList.vue
+++ src/components/toDoList.vue
@@ -1,120 +1,135 @@
 <script>
-    
-    let storeDatas;
-    // get array data from local storage
-    storeDatas = JSON.parse(localStorage.getItem('toDoList'));
-    
+let storeDatas;
+// get array data from local storage
+storeDatas = JSON.parse(localStorage.getItem("toDoList"));
 
-    export default {
-        name: 'ToDoList',
-        data() {
-            return {
-                task: '',
-                tasks: storeDatas ? storeDatas : [],
-                editedTask: null,
-            }
+export default {
+  name: "ToDoList",
+  data() {
+    return {
+      task: "",
+      tasks: storeDatas ? storeDatas : [],
+      editedTask: null,
+    };
+  },
 
-        },
+  methods: {
+    // submit task
+    submitTask() {
+      if (this.task.length === 0) {
+        return;
+      }
+      if (this.editedTask === null) {
+        this.tasks.push({
+          name: this.task,
+          status: "to-do",
+        });
+      } else {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      }
 
-        methods: {
-            // submit task
-            submitTask() {
+      // save array data in local storage
+      localStorage.setItem("toDoList", JSON.stringify(this.tasks));
 
-                if (this.task.length === 0) {
-                    return;
-                }
-                if(this.editedTask === null) {
-                    this.tasks.push({
-                        name: this.task,
-                        status: 'to-do',
-                    });
-                }
-                else {
-                    this.tasks[this.editedTask].name = this.task;
-                    this.editedTask = null;
-                }
+      // clear input
+      this.task = "";
+    },
 
-                // save array data in local storage
-                localStorage.setItem('toDoList', JSON.stringify(this.tasks));
-
-                // clear input
-                this.task = '';
-            },
-            
-            // delete task
-            deleteTask(index) {
-                this.tasks.splice(index, 1);
-                // delete array data from local storage
-                localStorage.setItem('toDoList', JSON.stringify(this.tasks));
-            },
-            // edit task
-            editTask(index) {
-                this.task = this.tasks[index].name;
-                this.editedTask = index;
-            }
-
-        }
-    }
- 
+    // delete task
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+      // delete array data from local storage
+      localStorage.setItem("toDoList", JSON.stringify(this.tasks));
+    },
+    // edit task
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+  },
+};
 </script>
 
 <template>
-
-    <div class="container">
-        <h2>My ToDo App</h2>
-        <div class="d-flex gap-2">
-            <input v-model="task" type="text" placeholder="Enter Value" class="form-control">
-            <button @click="submitTask" class="btn btn-warning rounded-1">Submit</button>
-        </div>
-        <!-- using bootstrap table -->
-        <table class="table table-bordered mt-5">
-            <thead>
-                <tr>
-                    <th scope="col">Task</th>
-                    <th scope="col">Type</th>
-                    <th scope="col" class="text-center">Edit</th>
-                    <th scope="col" class="text-center">Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(task, index) in tasks" :key="index">
-                    <td>{{task.name}}</td>
-                    <td>
-                        {{task.status}}
-                    </td>
-                    <td @click="editTask(index)" style="text-align: center;cursor: pointer;">
-                        <div>
-                            <img width="20" height="20" src="../components/pen.png" alt="pen">
-                        </div>
-                    </td>
-                    <td  style="text-align: center;cursor: pointer;" @click="deleteTask(index)"> 
-                        <div>
-                            <img width="20" height="20" src="../components/recycle.png" alt="pen">
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-
+  <div class="container">
+    <h2>My ToDo App</h2>
+    <form @submit.prevent="submitTask">
+      <div class="d-flex gap-2">
+        <input
+          v-model="task"
+          type="text"
+          placeholder="Enter Value"
+          class="form-control"
+        />
+        <button class="btn btn-warning rounded-1">Submit</button>
+      </div>
+    </form>
+    <!-- using bootstrap table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Type</th>
+          <th scope="col" class="text-center">Edit</th>
+          <th scope="col" class="text-center">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>{{ task.name }}</td>
+          <td>
+            {{ task.status }}
+          </td>
+          <td
+            @click="editTask(index)"
+            style="text-align: center; cursor: pointer"
+          >
+            <div>
+              <img
+                width="20"
+                height="20"
+                src="../components/pen.png"
+                alt="pen"
+              />
+            </div>
+          </td>
+          <td
+            style="text-align: center; cursor: pointer"
+            @click="deleteTask(index)"
+          >
+            <div>
+              <img
+                width="20"
+                height="20"
+                src="../components/recycle.png"
+                alt="pen"
+              />
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
-    tr:nth-child(even) {
-        background-color: #f2f2f2;
-    }
-    h2{
-        margin-top: 6rem;
-        margin-bottom: 2rem;
-    }
-    .ItemCenter{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        height: 100%;
-    }
-    .pointer{cursor: pointer;}
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+h2 {
+  margin-top: 6rem;
+  margin-bottom: 2rem;
+}
+.ItemCenter {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+.pointer {
+  cursor: pointer;
+}
 </style>
